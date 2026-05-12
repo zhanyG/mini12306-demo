9112cc770ec49b94cb712c131204fd24a76a13a4
zhanyG <3561759472@qq.com>
2026-05-12 14:58:40 +0800

    feat: initial project setup with README documentation

---
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+    //主函数，程序入口
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
